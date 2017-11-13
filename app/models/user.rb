9c47dd168adc4c0b8bb849411c4c585be227be52
ChenWang <bestjc0518@gmail.com>
2017-11-13 10:48:06 +0800

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save :email_downcase
+  attr_accessor :remember_token, :activation_token
+  before_save   :email_downcase
+  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -15,9 +16,15 @@ class User < ApplicationRecord
   end
 
   # 如果指定的令牌和摘要匹配， 返回 true
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  # def authenticated?(remember_token)
+  #   return false if remember_digest.nil?
+  #   BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  # end
+
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # 忘记用户
@@ -37,9 +44,24 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
-  private
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
 
+  # 激活用户需要更新的属性
+  def active
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  private
+    # 把电子邮件地址转换成小写
     def email_downcase
       email.downcase!
+    end
+
+    # 创建并赋值激活令牌和摘要
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
     end
 end
