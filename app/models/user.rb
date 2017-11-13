b7fe9cff27544345ade880af2e14f4967972a5b5
ChenWang <bestjc0518@gmail.com>
2017-11-14 04:34:23 +0800

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :email_downcase
   before_create :create_activation_digest
@@ -68,6 +69,10 @@ class User < ApplicationRecord
   # 如果密码重设请求超时了，返回 true
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
